ea54830906b92306bb37ebb4fdb0bc4def59eee2
Milad Khanlou <104443908+miladkhanlou@users.noreply.github.com>
2025-01-14 16:33:46 -0600

    Update merge_and_process.py

--- merge_and_process.py
+++ merge_and_process.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 # Global Variables, CSV file names
 updated_all_pids= "all-pids.csv"
@@ -9,13 +10,26 @@ obj_datastream_pids = "datastream-obj.csv"
 def get_pid_name(all_pids):
     all_pids = pd.read_csv(all_pids)
     PID = all_pids["PID"][0].split(":")[0]
-    return PID
+    institution = PID.split("-")[0]
+    return PID, institution
 
-def outpud_dir(PID):
-    output_file = f"full-{PID}.csv"
+
+def create_output_dir(PID, institution):
+    # load all collection data
+    all_collection_df = pd.read_csv(f"institutions/{institution}/institution_data.csv")
+    # Get the row of our PID
+    row = all_collection_df[all_collection_df["PID"] == f"{PID}:collection"]
+    # Get Collection title
+    collection_title = row["collection_title"].values[0]
+    # Create Output directory and make directory for the colleciton with collection title
+    output_dir = f"institutions/{institution}/{collection_title}"
+    os.makedirs(output_dir, exist_ok=True)
+    # Construct output file
+    output_file = os.path.join(output_dir, f"{PID}.csv")
     return output_file
 
-def merge_data(all_pids, pdf_pids, obj_pids, PID):
+
+def merge_data(all_pids, pdf_pids, obj_pids):
     # Load CSVs
     all_pids = pd.read_csv(all_pids)
     pdf_pids = pd.read_csv(pdf_pids)
@@ -61,13 +75,11 @@ def merge_data(all_pids, pdf_pids, obj_pids, PID):
     return final_df
 
 def to_csv(final_df, output_file):
-    # Save the final output
     final_df.to_csv(output_file, index=False)
 
 def filter_datastreams(raw_data):
     output_rows = []
-    csv = pd.read_csv(raw_data)
-    for _, row in csv.iterrows():
+    for _, row in raw_data.iterrows():
         base_row = row.to_dict()
         parent_pid_name = base_row["parent_PID"]
         pid_name = base_row["PID"]
@@ -95,34 +107,29 @@ def filter_datastreams(raw_data):
     return processed_df 
 
 def post_process(df):
-    # Identify rows that filetype is is empty:
-    rows_to_drop = df[(df.duplicated(subset=['PID'], keep='first')) & (df['filetype'] == "")]
-    
     # Drop the identified rows
     cleaned_df = df[~((df.duplicated(subset=['PID'], keep='first')) & (df['filetype'] == ""))]
     return cleaned_df
     
-def merge():
-    PID = get_pid_name(updated_all_pids)
-    output = outpud_dir(PID)
-    merged_df = merge_data(updated_all_pids, pdf_datastream_pids, obj_datastream_pids, PID)
-    to_csv(merged_df, f"collections/{PID}/merged-{output}")
-    print(f"Merged data saved as output-pre-processed-{output} ...")
 
-def process():
-    PID = get_pid_name(updated_all_pids)
-    output = outpud_dir(PID)
-    datastrean_filter_df = filter_datastreams(f"collections/{PID}/merged-{output}")
-    post_processed_df = post_process(datastrean_filter_df) # Process the DataFrame to separate rows to drop and cleaned data
-    to_csv(post_processed_df, f"collections/{PID}/{output}") # Save the cleaned DataFrame
+def merge_and_process():
+    # Construct PID, Institution name and Directories
+    PID, institution = get_pid_name(updated_all_pids)
+    output_file = create_output_dir(PID, institution)
+
+    # Merge Step
+    merged_csv= merge_data(updated_all_pids, pdf_datastream_pids, obj_datastream_pids)
     
-    print(f"Final Accounting saved as output-processed-{output} ...\n-----------------------------------------------------------------------------------")
+    # Process step
+    datastrean_filter_df = filter_datastreams(merged_csv)
+    post_processed_df = post_process(datastrean_filter_df) # Process the DataFrame to separate rows to drop and cleaned data
+    
+    # Save to csv
+    to_csv(post_processed_df, output_file)  # Save the cleaned DataFrame
+    print(f"Final Accounting saved as output-processed-{output_file} ...\n-----------------------------------------------------------------------------------")
 
 if __name__ == "__main__":
     print("Start Merging and Processing Data...\n")   
-    merge()
-    process()
+    merge_and_process()
     
 # To test if a row had 2 duplicated rows and we wanted to keep the ones that filetype is not none, in output-pre-processed make changes to the row with both pdf and obj to make pdf row to newspaperIssueCmodel 
-    
-    
